02705ee90773113b74297e0d15db733b1efe7375
avetyan-gayane <avetyan.gayane.01@gmail.com>
2023-07-16 02:27:25 +0400

    fix

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
-delete from student where id in (select student_id from mark where mark > 4)
-delete from student where id in (select student_id from mark where mark < 4);
+delete from student where id in (select distinct student_id from mark where mark >= 4)
+delete from student where id in (select distinct student_id from mark where mark < 4);
 delete from paymenttype where name = 'Daily';
-delete from mark where mark < 7;
+delete from mark m where m.mark < 7;
